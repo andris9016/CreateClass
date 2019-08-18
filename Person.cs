91120251cb5cc77a13ffc6b8744130401c2a2f74
Molnar Andras <andris9016@gmail.com>
2019-08-18 11:15:09 +0200

    Add some documentation

--- Person.cs
+++ Person.cs
@@ -6,12 +6,22 @@ using System.Threading.Tasks;
 
 namespace CreateClass
 {
+    ///<summary>
+    ///The main <c>Person</c> class.
+    ///Containing all fields and methods about this class
+    ///</summary>
+    ///<remarks>
+    ///Person has a name, birth date, and gender
+    ///</remarks>
     class Person
     {
 
         private Gender gender;
         private DateTime birthDate;
 
+        /// <value>
+        /// Gets value of the person name, and sets it.
+        /// </value>
         public string Name { get; set; }
 
         public DateTime BirthDate {
@@ -35,6 +45,10 @@ namespace CreateClass
             Gender = gender;
         }
 
+        /// <summary>
+        /// Overriding the original ToSTring method.
+        /// </summary>
+        /// <returns>string</returns>
         public override string ToString()
         {
             return string.Format("The person name: {0}, birth date: {1}, gender: {2}", Name, BirthDate, Gender);
